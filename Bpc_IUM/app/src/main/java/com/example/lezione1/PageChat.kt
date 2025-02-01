7bffc28b60fe69b39945b0eaebf57e9161a56ce1
BettyPu3 <elis3pu@gmail.com>
2025-02-01 15:02:13 +0100

    aggiunta toolbar kt

--- Bpc_IUM/app/src/main/java/com/example/lezione1/PageChat.kt
+++ Bpc_IUM/app/src/main/java/com/example/lezione1/PageChat.kt
@@ -1,4 +1,107 @@
 package com.example.lezione1
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class PageChat {
+class PageChat : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.page_chat)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val menuIcon: ImageView = findViewById(R.id.menu_icon)
+
+        menuIcon.setOnClickListener { view ->
+            showPopupMenu(view)
+        }
+
+        /**
+         * dovrebbe far funzionare la navbar, non funziona perché ho collegato tutto con logout
+         * per fare un check veloce e perché non abbiamo le pagine necessarie
+         **/
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navbar)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    // Azione per Home
+                    home()
+                    true
+                }
+                R.id.navigation_search -> {
+                    // Azione per Cerca
+                    logout()
+                    true
+                }
+                R.id.navigation_profile -> {
+                    // Azione per Profilo
+                    profile()
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+
+    fun logout(){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun home(){
+        val intent = Intent(this,SecondActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun profile(){
+        val intent = Intent(this,ProfileCliente::class.java)
+        startActivity(intent)
+    }
+
+    fun showPopupMenu(view: View) {
+        val popup = PopupMenu(this, view)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.toolbar_menu, popup.menu)
+
+        // Gestire il click sugli elementi del menu
+        popup.setOnMenuItemClickListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.aaaa -> {
+                    true
+                }
+                R.id.modifica -> {
+                    true
+                }
+                R.id.logout -> {
+                    logout()
+                    true
+                }
+                else -> false
+            }
+        }
+        popup.show()
+    }
 }
--- Bpc_IUM/app/src/main/java/com/example/lezione1/SecondActivity.kt
+++ Bpc_IUM/app/src/main/java/com/example/lezione1/SecondActivity.kt
@@ -2,13 +2,18 @@ package com.example.lezione1
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -26,6 +31,13 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val menuIcon: ImageView = findViewById(R.id.menu_icon)
+
+        menuIcon.setOnClickListener { view ->
+            showPopupMenu(view)
+        }
+
         /**
          * dovrebbe far funzionare la navbar, non funziona perché ho collegato tutto con logout
          * per fare un check veloce e perché non abbiamo le pagine necessarie
@@ -66,5 +78,29 @@ class SecondActivity : AppCompatActivity() {
     fun profile(){
         val intent = Intent(this,ProfileCliente::class.java)
         startActivity(intent)
+    }
+
+    fun showPopupMenu(view: View) {
+        val popup = PopupMenu(this, view)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.toolbar_menu, popup.menu)
+
+        // Gestire il click sugli elementi del menu
+        popup.setOnMenuItemClickListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.aaaa -> {
+                    true
+                }
+                R.id.modifica -> {
+                    true
+                }
+                R.id.logout -> {
+                    logout()
+                    true
+                }
+                else -> false
+            }
+        }
+        popup.show()
     }
 }
